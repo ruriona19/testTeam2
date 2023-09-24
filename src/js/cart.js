999b5b29bda09c00256bbf8320fff62f692220ed
Roberto Uriona <roberto.uriona@jalasoft.com>
2023-09-24 18:15:07 -0400

    Fixing console error related to Add to Cart button

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,13 @@
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  try {
+    const cartItems = getLocalStorage("so-cart") || [];
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  } catch (error) {
+    alert(`Error Name: ${error.name} \nError Message: ${error.message}`);
+  }
 }
 
 function cartItemTemplate(item) {
