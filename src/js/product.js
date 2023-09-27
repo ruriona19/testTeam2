dccc3c4dba7308d283b9ff0bfa3ed104530ea185
Roberto Uriona <roberto.uriona@jalasoft.com>
2023-09-27 17:50:34 -0400

    Added ProductDeatils page

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,5 @@
-import { setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
-import { getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  try {
-    // Get the current cart items from local storage or initialize an empty array
-    let cartItems = getLocalStorage("so-cart") || [];
-    cartItems.push(product);
-    setLocalStorage("so-cart", cartItems);
-  } catch (error) {
-    alert(`Error Name: ${error.name} \nError Message: ${error.message}`);
-  }
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId);
